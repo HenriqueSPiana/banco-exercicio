ad90404157bc5fefddf68474fa685aeb4db73709
HenriqueSPiana <103382089+HenriqueSPiana@users.noreply.github.com>
2025-09-17 18:29:11 -0300

    implementa a interface e variavel TipoConta

--- src/Banco/ContaBancaria.java
+++ src/Banco/ContaBancaria.java
@@ -1,8 +1,19 @@
 package Banco;
 
-public abstract class ContaBancaria {
+public abstract class ContaBancaria implements Conta{
 
     public String dono;
     public double saldo;
+    public TipoConta tipoConta;
 
+
+    @Override
+    public void consultarSaldo() {
+
+    }
+
+    @Override
+    public void depositar(double valor) {
+
+    }
 }
